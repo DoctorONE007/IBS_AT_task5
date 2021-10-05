0649e4312895f2cd432e1d4e84aad7768b0aa6f3
DoctorONE007 <drone@pswd.ru>
2021-10-05 15:00:06 +0300

    Add task 5

--- Jenkinsfile_scripted.groovy
+++ Jenkinsfile_scripted.groovy
@@ -1,33 +1,33 @@
 def mvn = "/var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/3.6.3/bin/mvn"
-node("label"){labell 'linux'}
-node {
 
-    label 'linux'
-    stage('Checkout SCM') {
-        checkout(
-                [$class           : 'GitSCM',
-                 branches         : [[name: "refs/heads/${BRANCH}"]],
-                 userRemoteConfigs: [[url: 'https://github.com/DoctorONE007/IBS_AT_task5.git']]]
-        )
-    }
-    stage('Build') {
-        sh "${mvn} clean compile"
-    }
-    stage('Run Tests') {
-        try {
-            sh "${mvn} test -Dcucumber.filter.tags=\"${TAG}\""
+node("linux") {
+    node {
+        stage('Checkout SCM') {
+            checkout(
+                    [$class           : 'GitSCM',
+                     branches         : [[name: "refs/heads/${BRANCH}"]],
+                     userRemoteConfigs: [[url: 'https://github.com/DoctorONE007/IBS_AT_task5.git']]]
+            )
         }
-        catch (Exception e) {
-            echo "Test run was broken"
-            throw e
+        stage('Build') {
+            sh "${mvn} clean compile"
         }
-        finally {
-            stage('Allure Report Generation') {
-                allure includeProperties: false,
-                        jdk: '',
-                        results: [[path: 'target/reports/allure-results']]
+        stage('Run Tests') {
+            try {
+                sh "${mvn} test -Dcucumber.filter.tags=\"${TAG}\""
             }
-            cleanWs notFailBuild: true
+            catch (Exception e) {
+                echo "Test run was broken"
+                throw e
+            }
+            finally {
+                stage('Allure Report Generation') {
+                    allure includeProperties: false,
+                            jdk: '',
+                            results: [[path: 'target/reports/allure-results']]
+                }
+                cleanWs notFailBuild: true
+            }
         }
     }
 }
