1cdbcea8387fd35a024d427c610328196d8910bd
DoctorONE007 <drone@pswd.ru>
2021-10-05 15:03:22 +0300

    Add task 5

--- Jenkinsfile_scripted.groovy
+++ Jenkinsfile_scripted.groovy
@@ -1,34 +1,33 @@
 def mvn = "/var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/3.6.3/bin/mvn"
 
 
-    node {
-        stage('Checkout SCM') {
-            node("linux") {
-                checkout(
-                        [$class           : 'GitSCM',
-                         branches         : [[name: "refs/heads/${BRANCH}"]],
-                         userRemoteConfigs: [[url: 'https://github.com/DoctorONE007/IBS_AT_task5.git']]]
-                )
-            }
+node(label: 'linux' {
+    stage('Checkout SCM') {
+        checkout(
+                [$class           : 'GitSCM',
+                 branches         : [[name: "refs/heads/${BRANCH}"]],
+                 userRemoteConfigs: [[url: 'https://github.com/DoctorONE007/IBS_AT_task5.git']]]
+        )
+
+    }
+    stage('Build') {
+        sh "${mvn} clean compile"
+    }
+    stage('Run Tests') {
+        try {
+            sh "${mvn} test -Dcucumber.filter.tags=\"${TAG}\""
         }
-        stage('Build') {
-            sh "${mvn} clean compile"
+        catch (Exception e) {
+            echo "Test run was broken"
+            throw e
         }
-        stage('Run Tests') {
-            try {
-                sh "${mvn} test -Dcucumber.filter.tags=\"${TAG}\""
+        finally {
+            stage('Allure Report Generation') {
+                allure includeProperties: false,
+                        jdk: '',
+                        results: [[path: 'target/reports/allure-results']]
             }
-            catch (Exception e) {
-                echo "Test run was broken"
-                throw e
-            }
-            finally {
-                stage('Allure Report Generation') {
-                    allure includeProperties: false,
-                            jdk: '',
-                            results: [[path: 'target/reports/allure-results']]
-                }
-                cleanWs notFailBuild: true
-            }
+            cleanWs notFailBuild: true
         }
     }
+}
